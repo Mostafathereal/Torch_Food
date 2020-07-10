8964a7c0f0a06496fa2f160ddba50e89fb57d06f
Mostafa Mohsen <reachmostafa.m@gmail.com>
2020-07-09 23:15:00 -0400

    defining nn class

--- warm_ups/define_net.py
+++ warm_ups/define_net.py
@@ -1,3 +1,4 @@
+import cv2
 
 import torch 
 import torch.nn as nn
@@ -19,4 +20,35 @@ class Net(nn.module):
         self.fc1 = nn.Linear(16 * 6 * 6, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        
+        # first block: x --> conve1 --> relu --> max_ppol
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        
+        # second block
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+
+        # reshaping the tensor to #batches by #features in 1 sample
+        x = x.view(-1, self.num_flat_features(x))
+
+        # first fully connected layer
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
+
+        # softmax or whatever output activation function will be applied later if necessary
+        x = self.fc3(x)
+
+        return x
+
+    # helper  method for flattening the last conv layer into a fc layer
+    def num_flat_features(self, x):
+        size = x.size()[1:] # exclude batch dimension
+        num_features = 1
+        for i in size():
+            num_features *= i
+        return num_features
+
+net = Net()
+print(net)
 
